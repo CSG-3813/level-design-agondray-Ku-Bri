004a9b3cfcc5e8a5392b3de3faebe7366d8c639b
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2022-11-28 14:01:45 -0600

    fixed door animations and sounds and saved as scene 17

--- UnityAgonDray/Assets/Script/DoorSwing.cs
+++ UnityAgonDray/Assets/Script/DoorSwing.cs
@@ -25,7 +25,7 @@ public class DoorSwing : MonoBehaviour
     {
         if(other.tag == "Player")
         {
-            //playHinge();
+            playHinge();
             doorSwing.SetBool("IsOpen", true);
         }
     }
@@ -35,7 +35,7 @@ public class DoorSwing : MonoBehaviour
     {
         if (other.tag == "Player")
         {
-            //playHinge();
+            playHinge();
             doorSwing.SetBool("IsOpen", false);
         }
     }
