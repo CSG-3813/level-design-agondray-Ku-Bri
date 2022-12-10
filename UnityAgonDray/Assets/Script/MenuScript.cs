c2af51294f863ccd6a6262475e0ac1e61e6e3287
Ku-Bri <106645400+Ku-Bri@users.noreply.github.com>
2022-12-10 14:46:04 -0600

    Added MenuScene with Menuscript
    Added start and exit buttons

---
+++ UnityAgonDray/Assets/Script/MenuScript.cs
@@ -0,0 +1,37 @@
+/****
+ * Created by: Bridget Kurr
+ * Date Created: Nov 05, 2022
+ * 
+ * Last Edited:
+ * Last Edited by:
+ ****/
+
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+
+    [Header("Text Boxes")]
+    public TMP_Text titleTextbox; //textbox for the title
+    public TMP_Text creditsTextbox; //textbox for the credits
+    public TMP_Text copyrightTextbox; //textbox for the copyright
+    public TMP_Text endMessageTextbox; //textbox for the end message
+
+    void Start()
+    {
+        //Cursor.visible = true; 
+    }
+
+    public void OnGameStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void OnGameExit()
+    {
+        Application.Quit();
+    }
+}
+
